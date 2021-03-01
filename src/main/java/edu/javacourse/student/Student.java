bdbeaa2d431aa09e1dd281ba4712882f1b08eaf8
sergei1733 <sergei1733@yandex.ru>
2021-03-01 23:40:36 +0500

    new project student

--- src/main/java/edu/javacourse/student/Student.java
+++ src/main/java/edu/javacourse/student/Student.java
@@ -1,4 +1,32 @@
 package edu.javacourse.student;
 
+import org.hibernate.service.spi.InjectService;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Table(name = "sr_student")
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long studentId;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "middle_name")
+    private String middleName;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    @Column(name = "passport_seria")
+    private LocalDate passportSeria;
+    @Column(name = "passport_number")
+    private String passportNumber;
+    @Column(name = "passport_date")
+    private LocalDate passportDate;
+
+
+
 }
---
+++ src/main/java/edu/javacourse/student/University.java
@@ -0,0 +1,4 @@
+package edu.javacourse.student;
+
+public class University {
+}
