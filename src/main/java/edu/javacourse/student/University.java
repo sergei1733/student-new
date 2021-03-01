e0b3753b9c93810f96b6aa5db2a6429a976be0e7
sergei1733 <sergei1733@yandex.ru>
2021-03-01 23:46:41 +0500

    new project student

--- src/main/java/edu/javacourse/student/University.java
+++ src/main/java/edu/javacourse/student/University.java
@@ -1,4 +1,18 @@
 package edu.javacourse.student;
 
+import javax.persistence.*;
+
+@Table(name ="sr_university")
+@Entity
 public class University {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "university_id")
+    private Long universityId;
+    @Column(name = "university_name")
+    private String universityName;
+    @Column(name = "")
+
+
 }
