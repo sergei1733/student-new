9ed43d91698d6700e55d7e31e4f311216d31c840
sergei1733 <sergei1733@yandex.ru>
2021-03-03 23:27:11 +0500

    new project student

---
+++ src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -0,0 +1,11 @@
+package edu.javacourse.student.rest;
+
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Produces;
+
+@Component
+@Produces("/student")
+public class StudentController {
+
+}
---
+++ src/main/java/edu/javacourse/student/view/StudentResponse.java
@@ -0,0 +1,6 @@
+package edu.javacourse.student.view;
+
+public class StudentResponse {
+
+
+}
