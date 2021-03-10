0414ea16cadee21cd2e5d0a6b69ad6b120900c81
sergei1733 <sergei1733@yandex.ru>
2021-03-10 23:23:10 +0500

    new

--- src/main/java/edu/javacourse/student/rest/StudentController.java
+++ src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -1,11 +1,31 @@
 package edu.javacourse.student.rest;
 
+import edu.javacourse.student.domain.Student;
+import edu.javacourse.student.service.StudentService;
+import edu.javacourse.student.view.StudentRequest;
+import edu.javacourse.student.view.StudentResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+import java.util.List;
 
 @Component
 @Produces("/student")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<StudentResponse> getStudentInfo(StudentRequest request){
+        return studentService.getStudentInfo(request);
+     }
 
 }
--- src/main/java/edu/javacourse/student/view/StudentResponse.java
+++ src/main/java/edu/javacourse/student/view/StudentResponse.java
@@ -1,6 +1,69 @@
 package edu.javacourse.student.view;
 
+import edu.javacourse.student.domain.Faculty;
+import edu.javacourse.student.domain.Student;
+import edu.javacourse.student.domain.StudentForm;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.time.LocalDate;
+
 public class StudentResponse {
 
+    private String documentNumber;
+    @XmlJavaTypeAdapter(value = LocalDateAdapter.class)
+    private LocalDate documentDate;
+    @XmlJavaTypeAdapter(value = LocalDateAdapter.class)
+    private LocalDate expiredDate;
+    private String universityName;
+    private String facultyName;
+    private String studentForm;
 
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public LocalDate getDocumentDate() {
+        return documentDate;
+    }
+
+    public void setDocumentDate(LocalDate documentDate) {
+        this.documentDate = documentDate;
+    }
+
+    public LocalDate getExpiredDate() {
+        return expiredDate;
+    }
+
+    public void setExpiredDate(LocalDate expiredDate) {
+        this.expiredDate = expiredDate;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public String getFacultyName() {
+        return facultyName;
+    }
+
+    public void setFacultyName(String facultyName) {
+        this.facultyName = facultyName;
+    }
+
+    public String getStudentForm() {
+        return studentForm;
+    }
+
+    public void setStudentForm(String studentForm) {
+        this.studentForm = studentForm;
+    }
 }
