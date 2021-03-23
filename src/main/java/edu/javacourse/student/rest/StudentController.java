0019f6dc8ba2cc503a000eeb36a85f030b834ae7
sergei1733 <sergei1733@yandex.ru>
2021-03-23 22:58:14 +0500

    mvs

--- src/main/java/edu/javacourse/student/rest/StudentController.java
+++ src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -4,25 +4,29 @@ import edu.javacourse.student.service.StudentService;
 import edu.javacourse.student.view.StudentRequest;
 import edu.javacourse.student.view.StudentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Component
-@Produces("/student")
+@RestController
+@RequestMapping("/student")
 public class StudentController {
 
     @Autowired
     private StudentService studentService;
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<StudentResponse> getStudentInfo(StudentRequest request){
+
+    //    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+    public List<StudentResponse> getStudentInfo(StudentRequest request) {
         return studentService.getStudentInfo(request);
-     }
+    }
+
+    @GetMapping(path = "/check")
+    public String checkAdmin() {
+        return "Rest service is working";
+    }
 
 }
