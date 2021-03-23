bf81cb88e454f3a370e81cf19a3e8101b0148ba0
sergei1733 <sergei1733@yandex.ru>
2021-03-23 21:20:20 +0500

    140

--- src/main/java/edu/javacourse/student/servlet/UniversityListServlet.java
+++ src/main/java/edu/javacourse/student/servlet/UniversityListServlet.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @WebServlet(name = "UniversityListServlet", urlPatterns = {"/universityList"})
@@ -21,10 +23,12 @@ public class UniversityListServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletContext ctx = getServletContext();
         WebApplicationContext webCtx = WebApplicationContextUtils.getWebApplicationContext(ctx);
+
         UniversityService service = webCtx.getBean(UniversityService.class);
         List<University> list = service.findUniversities();
-        list.forEach(u -> System.out.println(
-                u.getUniversityId()+ ":" + u.getUniversityName() + ":"+ u.getFaculties().size()));
+
+        req.setAttribute("today", LocalDate.now().format(DateTimeFormatter.ISO_DATE));
+        req.setAttribute("universities", list);
 
         getServletContext().getRequestDispatcher("/universityList.jsp").forward(req, resp);
     }
