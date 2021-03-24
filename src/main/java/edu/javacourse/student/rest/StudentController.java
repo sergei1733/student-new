7cf166658d112124d4ee2ba6bb925e6aacb13840
sergei1733 <sergei1733@yandex.ru>
2021-03-24 23:57:37 +0500

    spring mvs

--- src/main/java/edu/javacourse/student/rest/StudentController.java
+++ src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -6,7 +6,10 @@ import edu.javacourse.student.view.StudentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -30,6 +33,18 @@ public class StudentController {
     public String checkParams(@PathVariable("checkId") Long checkId,
                               @RequestParam("comment") String comment){
         return checkId + ":" + comment;
+    }
+
+    @PostMapping(path = "/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String uploadPhoto(@RequestParam("comment") String comment,
+                              @RequestParam("photoFile")MultipartFile photoFile){
+        try(InputStream is = photoFile.getInputStream()){
+            return "Comment:" + comment + " Name:"+ photoFile.getName()
+                    + " File Name:"+ photoFile.getOriginalFilename() + ", Size:" + is.available();
+        } catch (IOException ex){
+            throw new RuntimeException(ex);
+        }
+
     }
 
 }
